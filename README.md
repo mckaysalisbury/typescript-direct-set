fd9014a587b1ae9baf9a0946201477895643c4b2
McKay Salisbury <mckay@dave.com>
2020-03-04 17:26:51 -0800

    clarity

---
+++ README.md
@@ -0,0 +1,7 @@
+This seems to me to be undesirable behavior in typescript. It should know that the objects can't be null.
+
+To reproduce:
+
+    tsc
+
+The file `repro.ts` has some comments explaining the behavior.
--- repro.ts
+++ repro.ts
@@ -1,11 +1,27 @@
 interface I {
-  t?: (I | null);
-  a: number;
+  optional?: (I | null);
+  required: number;
 }
 
-const i: I = {
-  t: { a: 2 },
-  a: 1
+const w: I = {
+  optional: { required: 2 },
+  required: 1
 };
 
-i.t.a
+w.optional.required; // I can kind of see why this complains. I explicitly declared that the type of the variable at the time of setting can be nullable
+
+
+let workaround: I;
+workaround = {
+  optional: { required: 2 },
+  required: 1
+};
+
+workaround.optional.required;  // This one I think is wrong. I'm explicitly setting the 
+
+const workaroundToWorkaround: I = {
+  required: 1,
+};
+workaroundToWorkaround.optional = { required: 1 }; // So I have to explicitly state that the value of this property is not null to get the intended behavior
+
+workaroundToWorkaround.optional.required; // This one doesn't have a problem
